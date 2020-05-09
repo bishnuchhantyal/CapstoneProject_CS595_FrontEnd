e8ef27e91f1bd71ed83c737bce6c6ec41b97b1a5
bijaya12345 <47364203+bijaya12345@users.noreply.github.com>
2020-05-09 12:56:58 -0500

    Update cartActions.js

--- src/src/store/actions/cartActions.js
+++ src/src/store/actions/cartActions.js
@@ -13,15 +13,7 @@ export const addToCart = (product) => {
     }
 };
 
-export const removeFromCart = (productId) => {
 
-    return {
-        type: 'REMOVE_FROM_CART',
-        payload: {
-            productId: productId
-        }
-    }
-};
 
 export const updateCartQuantity = (productId, quantity) => {
 
